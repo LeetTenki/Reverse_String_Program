e84cf55110aed4c6a87570e37a684085e7b025a7
Leeto <boshoabotsoere7@gmail.com>
2023-06-27 14:57:54 +0200

    uploaded complete project

---
+++ Reverse_String_Program.py
@@ -0,0 +1,10 @@
+# Developed by Leeto Tenki 2023
+
+"""
+This program just reverse a string and Display it.
+"""
+
+if __name__ == '__main__':
+    string_input = input("Enter a phrase, word or anything: ")
+    reversed_string = string_input[::-1]
+    print("The reversed input is: {}".format(reversed_string))
